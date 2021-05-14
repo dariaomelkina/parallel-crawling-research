17df4a11dfde5308a45054912a5464193d67bdf0
maxymkuz <maxymkuz@gmail.com>
2021-05-14 21:09:42 +0300

    Simplified benchmark

--- google-benchmark/main.cpp
+++ google-benchmark/main.cpp
@@ -6,30 +6,13 @@ static void BM_StringCreation(benchmark::State& state) {
         state.PauseTiming();
         // preparing data if needed here
         state.ResumeTiming();
-        // feel free to call your function here and benchmark it
-//        std::cout << 'dskfkdsf' << std::endl;
-        for (int i = 0; i < state.range(0); ++i) {
-            std::string empty_string;
-        }
+
+        // тут
     }
 }
 // Register the function as a benchmark and passing an argument, number of iterations as a constraint
-// BENCHMARK(BM_StringCreation)->Args({10, 0})->Arg(100)->Arg(1000)->Iterations(100);
+ BENCHMARK(BM_StringCreation)->Iterations(1000);
 // Time in seconds, as a constraint
-BENCHMARK(BM_StringCreation)->Args({10, 0})->Arg(100)->Arg(1000)->MinTime(1);
-
-//// function to benchmark multiple thread programs
-//static void BM_MultiThreaded(benchmark::State& state) {
-//    if (state.thread_index == 0) {
-//        // Setup code here.
-//    }
-//    for (auto _ : state) {
-//        // Run the test as normal.
-//    }
-//    if (state.thread_index == 0) {
-//        // Teardown code here.
-//    }
-//}
-//BENCHMARK(BM_MultiThreaded)->Threads(2);
+//BENCHMARK(BM_StringCreation)->Args({10, 0})->Arg(100)->Arg(1000)->MinTime(1);
 
 BENCHMARK_MAIN();
