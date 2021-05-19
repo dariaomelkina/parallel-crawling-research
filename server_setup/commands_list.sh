fd8074d4915e5b644481cc3663b708f8074c9969
maxymkuz <maxymkuz@gmail.com>
2021-05-19 23:29:46 +0300

    Finished to setup and written instructions for the web server. Ready for benchmarks now

---
+++ server_setup/commands_list.sh
@@ -0,0 +1,29 @@
+echo "Hello, googlers!"
+
+
+#        IP ADDRESS INFORMATION:
+# get the proper address for web server:
+# nmcli -p device show
+
+# to get local ip address
+# ip address
+
+# to list public ip address:
+# sudo curl -s https://icanhazip.com 
+
+
+
+# RAM DISK setup:
+sudo mkdir -p /mnt/ramdisk
+sudo mount -t tmpfs -o rw,size=2G tmpfs /mnt/ramdisk
+
+
+# copy files to the ram disk as it is volatile
+sudo cp -r /home/google/test-websites /mnt/ramdisk
+
+
+# start nginx server. Note that nginx configuration is already done accoring to Daria's tutorial
+sudo systemctl start nginx
+
+echo "Setup is done. Now we are able to accept requests"
+
