8893f72a81f089d0f2733dd9568bf5b7970b548b
Daria Omelkina <odasha2001@gmail.com>
2021-05-28 16:44:08 +0300

    Added a bit more documentation for plotting,

--- results-processing/one_plot.py
+++ results-processing/one_plot.py
@@ -5,7 +5,7 @@ import numpy
 import plotly.express as px
 import plotly.graph_objects as go
 
-NUMBER_OF_PARAMETER_ROWS = 9
+NUMBER_OF_PARAMETER_ROWS = 11  # First lines without needed data
 NUMBER_OF_WARNING_ROWS = 1
 NUMBER_Of_SKIP_ROWS = 5
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     ###################################################################################################################
     # Read csv files
     df = pd.read_csv(sys.argv[1],
-                           skiprows=range(0, NUMBER_OF_PARAMETER_ROWS))
+                     skiprows=range(0, NUMBER_OF_PARAMETER_ROWS))
     data = df['real_time'][NUMBER_Of_SKIP_ROWS:] / 1000000000
     ###################################################################################################################
     # Plot customizations
@@ -22,7 +22,10 @@ if __name__ == "__main__":
     ###################################################################################################################
     # Creating bar plot of the distribution
     fig = px.histogram(x=data,
-                       nbins=50,
+                       nbins=300,  # TODO: test which number suits more
                        title="Results distribution",
                        labels=custom_labels)
-    fig.write_image("result-plots/test-plot.png")
+
+    # Uncomment if You want to save graph
+    # fig.write_image("result-plots/test-plot.png")
+    fig.show()
