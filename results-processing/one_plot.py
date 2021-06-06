bd0fdad9414405da0ae029de067c71b5eff0430c
Daria Omelkina <odasha2001@gmail.com>
2021-06-06 14:02:31 +0300

    Added moving average.

--- results-processing/one_plot.py
+++ results-processing/one_plot.py
@@ -1,11 +1,12 @@
 import sys
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from scipy import stats
 import numpy
 from delete_outliers import delete_outliers
 
-NUMBER_OF_PARAMETER_ROWS = 11  # First lines without needed data
+NUMBER_OF_PARAMETER_ROWS = 0  # First lines without needed data
 # NUMBER_OF_WARNING_ROWS = 20
 NUMBER_Of_SKIP_ROWS = 0
 
@@ -51,9 +52,34 @@ if __name__ == "__main__":
                      "y": "Number of iterations"}
 
     ###################################################################################################################
+    # Plot moving average
+
+    window_size = 5
+    numbers_series = pd.Series(data_real_time)
+    windows = numbers_series.rolling(window_size)
+    moving_averages = windows.mean()
+    moving_averages_list = moving_averages.tolist()
+    without_nans = moving_averages_list[window_size - 1:]
+
+    fig_ma = go.Figure()
+    fig_ma.add_trace(go.Scatter(x=[i for i in range(len(without_nans))], y=without_nans,
+                                mode='lines',
+                                name=f'moving average ({window_size} iterations)'))
+
+    fig_ma.add_trace(go.Scatter(x=[i for i in range(len(data_real_time))], y=data_real_time,
+                                mode='lines',
+                                name='real time for each iteration'))
+
+    fig_ma.update_layout(title=f'For {sys.argv[1]}',
+                         xaxis_title='real time',
+                         yaxis_title='iteration')
+
+    fig_ma.show()
+
+    ###################################################################################################################
     # Creating bar plot of the distribution
     fig = px.histogram(x=data_real_time,
-                       nbins=100,  # TODO: test which number suits more
+                       nbins=len(data_real_time) + 3,
                        title=f"Results distribution of {sys.argv[1]} (real time)",
                        labels=custom_labels)
 
